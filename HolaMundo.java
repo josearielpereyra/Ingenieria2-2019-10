a1fdb58932c2ac43dc8824998603c86dc1ed1d16
ruddy <djmister_ruddy@hotmail.com>
2019-04-02 16:19:07 -0400

    Lo Mas Fuerte

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
     public static void main(String[] args) {
         System.out.println("Hola Mundo! Que tal?");
         System.out.println("IvanDroid");
+        System.out.println("Capa 8 Reload!");
     } 
 }
